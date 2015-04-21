f2ebc973009817925711e95de168bc92ba29be30
Maximilian Schmitt <maximilian.schmitt@googlemail.com>
2015-04-21 15:26:16 +0200

    refactors, gitclick.isEncrypted, gitclick tests

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
 'use strict';
 
 const path = require('path');
-const co = require('co');
 const assign = require('object.assign');
 const store = require('./lib/store');
+const git = require('./lib/git');
 const NoAccountSetError = require('./errors/no-account-set-error');
 const AlreadyEncryptedError = require('./errors/already-encrypted-error');
-const thenify = require('thenify');
-const exec = thenify(require('child_process').exec);
-let GithubProvider = require('gitclick-provider-github');
-let BitbucketProvider = require('gitclick-provider-bitbucket');
+const GithubProvider = require('gitclick-provider-github');
+const BitbucketProvider = require('gitclick-provider-bitbucket');
 
 const gitclick = function(storePath, password) {
   let s = store(storePath, password);
 
   return {
+    isEncrypted: function() {
+      const plaintextStore = store(storePath);
+
+      return plaintextStore.read().then(notEncrypted).catch(encrypted);
+
+      function encrypted() {
+        return true;
+      }
+
+      function notEncrypted() {
+        return false;
+      }
+    },
     encrypt: function() {
       const plaintextStore = store(storePath);
 
@@ -40,15 +51,29 @@ const gitclick = function(storePath, password) {
       return s.getAccount();
     },
     create: function(opts) {
-      return co(function*() {
-        const repository = opts.repository || path.basename(process.cwd());
-        const account = opts.account || (yield s.getAccount());
+      opts = opts || {};
+      let repository;
 
+      return getAccount()
+        .then(getAccountConfig)
+        .then(createRepository)
+        .then(function(result) { repository = result; })
+        .then(setRemote)
+        .then(function() { return repository; });
+
+      function getAccount() {
+        return opts.account ? Promise.resolve(opts.account) : s.getAccount();
+      }
+
+      function getAccountConfig(account) {
         if (!account) {
           throw new NoAccountSetError();
         }
 
-        const accountConfig = yield s.getAccountConfig(account);
+        return s.getAccountConfig(account);
+      }
+
+      function createRepository(accountConfig) {
         const Provider = accountConfig.provider === 'github' ? GithubProvider : BitbucketProvider;
 
         const provider = new Provider({
@@ -58,25 +83,26 @@ const gitclick = function(storePath, password) {
             password: accountConfig.password
           },
           defaults: {
-            wiki: opts.wiki,
-            issues: opts.issues,
-            private: opts.private
+            wiki: typeof opts.wiki === 'boolean' ? opts.wiki : true,
+            issues: typeof opts.issues === 'boolean' ? opts.issues : true,
+            private: typeof opts.private === 'boolean' ? opts.private : false
           }
         });
 
-        const repo = yield provider.createRepository({
-          name: repository,
-          wiki: opts.wiki,
-          issues: opts.issues,
-          private: opts.private
+        return provider.createRepository({
+          name: opts.repository || path.basename(process.cwd()),
+          wiki: typeof opts.wiki === 'boolean' ? opts.wiki : true,
+          issues: typeof opts.issues === 'boolean' ? opts.issues : true,
+          private: typeof opts.private === 'boolean' ? opts.private : false
         });
+      }
 
+      function setRemote() {
         if (opts.setRemote) {
-          yield exec(`git remote add ${opts.setRemote} ${repo.sshUrl}`, { cwd: process.cwd() });
+          const remote = opts.setRemote === true ? 'origin' : opts.setRemote;
+          return git(process.cwd()).setRemote(remote, repository.sshUrl);
         }
-
-        return repo;
-      });
+      }
     }
   };
 };
--- test/main.js
+++ test/main.js
@@ -1,25 +1,54 @@
 /* global describe, it */
 'use strict';
 
-const c = require('./helpers/test-configs');
-const rewire = require('rewire');
-const gitclick = rewire('../main');
-const chai = require('chai');
-const expect = chai.expect;
-const fs = require('thenify-all')(require('fs'));
-
-chai.use(require('chai-as-promised'));
-
 const ProviderMock = function(opts) {
-  this.opts = opts;
+  ProviderMock.opts = opts;
+  ProviderMock.repository = null;
 
-  this.createRepository = function(repository) {
-    this.repository = repository;
+  this.createRepository = function(opts) {
+    ProviderMock.repository = opts;
+
+    return Promise.resolve({
+      name: opts.name,
+      sshUrl: `https://provider/${opts.name}`,
+      cloneUrl: `https://provider/${opts.name}`
+    });
   };
 };
 
-gitclick.__set__('GithubProvider', ProviderMock);
-gitclick.__set__('BitbucketProvider', ProviderMock);
+const gitMock = function(cwd) {
+  gitMock.cwd = cwd;
+
+  return {
+    setRemote: function(name, url) {
+      gitMock.remoteName = name;
+      gitMock.remoteUrl = url;
+
+      return Promise.resolve();
+    }
+  };
+};
+
+gitMock.reset = function() {
+  gitMock.cwd = null;
+  gitMock.remoteName = null;
+  gitMock.remoteUrl = null;
+};
+
+const AccountNotFoundError = require('../errors/account-not-found-error');
+const NoAccountSetError = require('../errors/no-account-set-error');
+const c = require('./helpers/test-configs');
+const proxyquire = require('proxyquire');
+const chai = require('chai');
+const expect = chai.expect;
+const fs = require('thenify-all')(require('fs'));
+const gitclick = proxyquire('../main', {
+  'gitclick-provider-github': ProviderMock,
+  'gitclick-provider-bitbucket': ProviderMock,
+  './lib/git': gitMock
+});
+
+chai.use(require('chai-as-promised'));
 
 describe('gitclick', function() {
   it('exposes a factory function', function() {
@@ -29,6 +58,7 @@ describe('gitclick', function() {
   describe('encrypt', function() {
     it('encrypts the contents of the store', function() {
       const gc = gitclick(c.defaultConfigPath, c.password);
+
       return expect(gc.encrypt().then(readFile).then(toString))
         .to.eventually.equal('w/gYRs08GryHtq/bc/8/w3qzDTyUDwGAAKfAvobKzQs=');
 
@@ -43,7 +73,209 @@ describe('gitclick', function() {
 
     it('does not encrypt already encrypted stores', function() {
       const gc = gitclick(c.encryptedConfigPath, c.password);
-      return expect(gc.encrypt()).to.eventually.be.rejected;
+
+      return expect(gc.encrypt()).to.eventually.be.rejected.then(verifyContent);
+
+      function verifyContent() {
+        return expect(readFile().then(toString))
+          .to.eventually.equal('w/gYRs08GryHtq/bc/8/w3qzDTyUDwGAAKfAvobKzQs=');
+      }
+
+      function readFile() {
+        return fs.readFile(c.encryptedConfigPath);
+      }
+
+      function toString(buff) {
+        return buff.toString();
+      }
+    });
+  });
+
+  describe('isEncrypted', function() {
+    it('resolves to true if the store is encrypted', function() {
+      const gc = gitclick(c.encryptedConfigPath, c.password);
+
+      return expect(gc.isEncrypted())
+        .to.eventually.equal(true);
+    });
+
+    it('resolves to false if the store is not encrypted', function() {
+      const gc = gitclick(c.defaultConfigPath);
+
+      return expect(gc.isEncrypted())
+        .to.eventually.equal(false);
+    });
+  });
+
+  describe('create', function() {
+    it('throws AccountNotFoundError if provided account does not exist in store', function() {
+      const gc = gitclick(c.existingConfigPath);
+
+      return expect(gc.create({ account: 'non-existent' }))
+        .to.eventually.be.rejectedWith(AccountNotFoundError);
+    });
+
+    it('throws NoAccountSetError if no account is set in store', function() {
+      const gc = gitclick(c.defaultConfigPath);
+
+      return expect(gc.create({}))
+        .to.eventually.be.rejectedWith(NoAccountSetError);
+    });
+
+    it('creates repositories', function() {
+      const gc = gitclick(c.existingConfigPath);
+
+      return expect(gc.create({ repository: 'a-repository', account: 'private' }))
+        .to.eventually.deep.equal({
+          name: 'a-repository',
+          sshUrl: 'https://provider/a-repository',
+          cloneUrl: 'https://provider/a-repository'
+        });
+    });
+
+    it('creates repositories with issues by default', function() {
+      const gc = gitclick(c.existingConfigPath);
+
+      return expect(gc.create())
+        .to.eventually.be.fulfilled.then(checkIssues);
+
+      function checkIssues() {
+        expect(ProviderMock.repository.issues).to.equal(true);
+      }
+    });
+
+    it('creates repositories with a wiki by default', function() {
+      const gc = gitclick(c.existingConfigPath);
+
+      return expect(gc.create())
+        .to.eventually.be.fulfilled.then(checkWiki);
+
+      function checkWiki() {
+        expect(ProviderMock.repository.wiki).to.equal(true);
+      }
+    });
+
+    it('creates public repositories by default', function() {
+      const gc = gitclick(c.existingConfigPath);
+
+      return expect(gc.create())
+        .to.eventually.be.fulfilled.then(checkVisibility);
+
+      function checkVisibility() {
+        expect(ProviderMock.repository.private).to.equal(false);
+      }
+    });
+
+    it('creates repositories without issues if specified', function() {
+      const gc = gitclick(c.existingConfigPath);
+
+      return expect(gc.create({ issues: false }))
+        .to.eventually.be.fulfilled.then(checkIssues);
+
+      function checkIssues() {
+        expect(ProviderMock.repository.issues).to.equal(false);
+      }
+    });
+
+    it('creates repositories without a wiki if specified', function() {
+      const gc = gitclick(c.existingConfigPath);
+
+      return expect(gc.create({ wiki: false }))
+        .to.eventually.be.fulfilled.then(checkWiki);
+
+      function checkWiki() {
+        expect(ProviderMock.repository.wiki).to.equal(false);
+      }
+    });
+
+    it('creates private repositories if specified', function() {
+      const gc = gitclick(c.existingConfigPath);
+
+      return expect(gc.create({ private: true }))
+        .to.eventually.be.fulfilled.then(checkVisibility);
+
+      function checkVisibility() {
+        expect(ProviderMock.repository.private).to.equal(true);
+      }
+    });
+
+    it('configures the provider with the necessary auth information', function() {
+      const gc = gitclick(c.existingConfigPath);
+
+      return expect(gc.create())
+        .to.eventually.be.fulfilled.then(checkAuthConfig);
+
+      function checkAuthConfig() {
+        expect(ProviderMock.opts.auth).to.deep.equal({
+          type: 'basic',
+          username: 'private-username',
+          password: 'private-password'
+        });
+      }
+    });
+
+    it('uses default account if none provided', function() {
+      const gc = gitclick(c.existingConfigPath);
+
+      return expect(gc.create({ repository: 'a-repository' }))
+        .to.eventually.deep.equal({
+          name: 'a-repository',
+          sshUrl: 'https://provider/a-repository',
+          cloneUrl: 'https://provider/a-repository'
+        });
+    });
+
+    it('uses name of current folder if none provided', function() {
+      const gc = gitclick(c.existingConfigPath);
+
+      return expect(gc.create())
+        .to.eventually.deep.equal({
+          name: 'gitclick',
+          sshUrl: 'https://provider/gitclick',
+          cloneUrl: 'https://provider/gitclick'
+        });
+    });
+
+    it('sets remote if provided', function() {
+      gitMock.reset();
+      const gc = gitclick(c.existingConfigPath);
+
+      return expect(gc.create({ repository: 'a-repository', setRemote: 'develop' }))
+        .to.eventually.be.fulfilled.then(checkRemoteSet);
+
+      function checkRemoteSet() {
+        expect(gitMock.cwd).to.equal(process.cwd());
+        expect(gitMock.remoteName).to.equal('develop');
+        expect(gitMock.remoteUrl).to.equal('https://provider/a-repository');
+      }
+    });
+
+    it('sets remote as origin if not specified', function() {
+      gitMock.reset();
+      const gc = gitclick(c.existingConfigPath);
+
+      return expect(gc.create({ repository: 'a-repository', setRemote: true }))
+        .to.eventually.be.fulfilled.then(checkRemoteSet);
+
+      function checkRemoteSet() {
+        expect(gitMock.cwd).to.equal(process.cwd());
+        expect(gitMock.remoteName).to.equal('origin');
+        expect(gitMock.remoteUrl).to.equal('https://provider/a-repository');
+      }
+    });
+
+    it('does not set origin if not provided', function() {
+      gitMock.reset();
+      const gc = gitclick(c.existingConfigPath);
+
+      return expect(gc.create({ repository: 'a-repository' }))
+        .to.eventually.be.fulfilled.then(checkRemoteSet);
+
+      function checkRemoteSet() {
+        expect(gitMock.cwd).to.not.be.ok;
+        expect(gitMock.remoteName).to.not.be.ok;
+        expect(gitMock.remoteUrl).to.not.be.ok;
+      }
     });
   });
 });
